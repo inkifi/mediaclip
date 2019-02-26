f8002f4c80dcc2859518391b50a5d00e28f3ac20
Dmitry Fedyuk <dfediuk@gmail.com>
2019-02-26 16:45:36 +0700

    no message

--- lib/log.php
+++ lib/log.php
@@ -6,7 +6,7 @@ use Zend\Log\Writer\Stream as W;
  * @used-by \Inkifi\Mediaclip\H\AvailableForDownload::_p()
  * @used-by \Mangoit\MediaclipHub\Controller\Index\OneflowResponse::execute()
  * @used-by \Mangoit\MediaclipHub\Controller\Product\Edit::execute()
- * @used-by
+ * @used-by \Mangoit\MediaclipHub\Helper\Data::GetTokenForEndUser()
  * @used-by
  * @used-by
  * @used-by
