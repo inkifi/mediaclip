69d8308688685ef57150801f89eca394ad1850c1
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-11 20:52:43 +0600

    no message

--- lib/product.php
+++ lib/product.php
@@ -35,7 +35,7 @@ function ikf_product($p) {
  * 2019-03-04
  * @used-by \Mangoit\MediaclipHub\Controller\Product\Edit::getMediaclipProductData()
  * @used-by \Mangoit\MediaclipHub\Helper\Data::getMediaClipProductName()
- * @used-by \Mangoit\MediaclipHub\Model\Product::getMediaClipProductBySku()
+ * @used-by \Mangoit\MediaclipHub\Model\Product::bySku()
  * @return mPC
  */
 function ikf_product_c() {return df_new_om(mPC::class);}
