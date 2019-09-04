33ac352ccb4ad98484d4fc5835c96687a278cc60
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-04 14:36:48 +0600

    1.2.9

--- lib/product.php
+++ lib/product.php
@@ -1,5 +1,15 @@
 <?php
+use Magento\Catalog\Model\Product as P;
 use Mangoit\MediaclipHub\Model\ResourceModel\Product\Collection as mPC;
+/**
+ * 2019-09-04
+ * @used-by \Mangoit\MediaclipHub\Controller\Cart\Add::execute()
+ * @used-by app/design/frontend/Infortis/ultimo/Magento_Catalog/templates/product/view/addtocart.phtml
+ * @param P $p
+ * @return bool
+ */
+function ikf_is_mediaclip_product(P $p) {return 4 != $p->getAttributeSetId() && 'Map' !== df_att_set_name($p);}
+
 /**
  * 2019-03-04
  * @used-by \Mangoit\MediaclipHub\Controller\Product\Edit::getMediaclipProductData()
